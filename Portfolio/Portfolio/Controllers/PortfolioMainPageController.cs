7d72a1af88ba9b7d3297e0a3f5d63a6db6efff67
Olesya-Gerasimova <ogerasimova966@gmail.com>
2022-03-12 18:28:56 +0300

    added movie files

---
+++ Portfolio/Portfolio/Controllers/PortfolioMainPageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using System.Diagnostics;
+
+namespace Portfolio.Controllers
+{
+    public class PortfolioMainPageController : Controller
+    {
+        //private readonly ILogger<PortfolioMainPage> _logger;
+
+        //public PortfolioMainPage(ILogger<PortfolioMainPage> logger)
+        //{
+        //    _logger = logger;
+        //}
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+    }
+}
